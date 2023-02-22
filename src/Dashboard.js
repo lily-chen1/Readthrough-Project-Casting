9a0193abe95bc62bac1a70de2d47f83d8f4b4f3a
tinie doan <77512550+tinietdoan@users.noreply.github.com>
2023-02-22 11:46:28 -0700

    Update Dashboard.js

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,36 +1,125 @@
-import './Dashboard.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { useState, useEffect } from "react";
 
-function Dashboard() {
+import Data from './resources/mock_project_data.json'
+import './resources/ProjectDashboard.css';
+
+function ProjectDashboard() {
+	const [isArchived, archive_toggle] = useState(false);
+	
+	function handle_archive_toggle() {
+		if (isArchived == false) {
+			archive_toggle(true);
+		}
+		else {
+			archive_toggle(false);
+		}
+	}
+
+	// function handleHiatus(items) {
+	// 	const hiatus = [...new Set(items.map((e) => e.value))];
+	// 	if(item = ""){
+
+	// 	}
+	//   }
 return (
-	<div className="Dashboard">
-	<table>
-		<tr>
-		<th>Project Name</th>
-		<th>Owner</th>
-		<th>Status</th>
-        <th>Last Updated</th>
-		</tr>
-		<tr>
-		<td>Student Film</td>
-		<td>Logan Logan</td>
-		<td>Active</td>
-        <td>2/14/23</td>
-		</tr>
-		<tr>
-		<td>Lorem ipsum</td>
-		<td>dolor sit</td>
-		<td>Not Active</td>
-        <td>12/10/22</td>
-		</tr>
-		<tr>
-		<td>adipiscing elit</td>
-		<td>sed do eiusmod tempor</td>
-		<td>Active</td>
-        <td>1/0/23</td>
-		</tr>
-	</table>
-	</div>
+	<div className="container">
+		<div className="header">
+			<img className="logo" src={require('../writerDuet-logo.png')}></img>
+		</div>
+		<hr className="header-line"></hr>
+		<aside className="side-bar">
+				stuff
+				stuff
+				stuff
+		</aside>
+		<div className='project-table'>
+			<tb className="card-grid">
+			<tr>
+				<th>Project Name</th>
+				<th>Owner</th>
+				<th>Status</th>
+			</tr>	
+			{Data
+			.slice()
+				.map((item) => ( 	
+					<tr className="card">
+					<div className="card-content">
+						<div class="header">
+						<td>{item.name}</td>
+						<td>{item.owner}</td>
+						<td>{item.status}</td>
+						</div>
+					</div>
+					</tr>
+				))}
+			</tb>
+		</div>
+    </div>
 );
 }
 
-export default Dashboard;
+export default ProjectDashboard;import React from 'react';
+import ReactDOM from 'react-dom';
+import { useState, useEffect } from "react";
+
+import Data from './resources/mock_project_data.json'
+import './resources/ProjectDashboard.css';
+
+function ProjectDashboard() {
+	const [isArchived, archive_toggle] = useState(false);
+	
+	function handle_archive_toggle() {
+		if (isArchived == false) {
+			archive_toggle(true);
+		}
+		else {
+			archive_toggle(false);
+		}
+	}
+
+	// function handleHiatus(items) {
+	// 	const hiatus = [...new Set(items.map((e) => e.value))];
+	// 	if(item = ""){
+
+	// 	}
+	//   }
+return (
+	<div className="container">
+		<div className="header">
+			<img className="logo" src={require('../writerDuet-logo.png')}></img>
+		</div>
+		<hr className="header-line"></hr>
+		<aside className="side-bar">
+				stuff
+				stuff
+				stuff
+		</aside>
+		<div className='project-table'>
+			<tb className="card-grid">
+			<tr>
+				<th>Project Name</th>
+				<th>Owner</th>
+				<th>Status</th>
+			</tr>	
+			{Data
+			.slice()
+				.map((item) => ( 	
+					<tr className="card">
+					<div className="card-content">
+						<div class="header">
+						<td>{item.name}</td>
+						<td>{item.owner}</td>
+						<td>{item.status}</td>
+						</div>
+					</div>
+					</tr>
+				))}
+			</tb>
+		</div>
+    </div>
+);
+}
+
+export default ProjectDashboard;
