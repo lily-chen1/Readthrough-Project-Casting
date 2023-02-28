e685231f1ec0c10660f82257e7ee31ad20e5ea87
Lily Chen <autumnwindsky@gmail.com>
2023-02-28 11:27:31 -0700

    MobX test (WIP)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./Dashboard";
 import FirebaseTest from "./FirebaseTest";
+import MobXTest from "./MobXTest";
 
 const App = () => (
   <BrowserRouter>
@@ -9,6 +10,7 @@ const App = () => (
       {/* <Route path="/" element={<Home />} /> */}
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/firebasetest" element={<FirebaseTest />} />
+      <Route path="/mobxtest" element={<MobXTest />} />
     </Routes>
   </BrowserRouter>
 );
