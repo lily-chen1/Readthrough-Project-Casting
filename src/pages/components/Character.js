e09ac85b14dffd2f9077648f2ecf776f9f624cbb
Jamie Joung <jamiesjoung@gmail.com>
2023-03-08 01:49:23 -0700

    Able to add/edit Character details

--- src/pages/components/Character.js
+++ src/pages/components/Character.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useState, useRef } from "react";
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
@@ -15,102 +15,138 @@ import ToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import sampleProj from './project.json'
 
-// interface Chara  {
-//     ageRange: {
-//         high: Number,
-//         low: Number
-//     }
-// }
-const Character = ({prop})=> {
-    console.log(prop)
-    return(
-        <div>
-        <Grid container spacing={5}>
-                                <Grid item xs={2}>
-                                    <TextField
-                                        value={prop.characterName}
-                                    >
-                                    </TextField>
-                                </Grid>
-                                <Grid item xs={2}>
-                                    <ToggleButtonGroup
-                                        value={prop.type}
-                                        exclusive
-                                        //onChange={handleAlignment}
-                                        aria-label="text alignment">
-                                        <ToggleButton value="lead" aria-label="left aligned">
-                                            Lead
-                                        </ToggleButton>
-                                        <ToggleButton value="supporting" aria-label="centered">
-                                            Supporting
-                                        </ToggleButton>
-                                    </ToggleButtonGroup>
-                                </Grid>
-                                <Grid item xs={2}>
-                                    <FormGroup fullWidth>
-                                        <Select
-                                            labelId="demo-simple-select-label"
-                                            id="demo-simple-select"
-                                            value={prop.genderIdentity}
-                                            label="Gender"
-                                           
-                                        >
-                                            <MenuItem value={"female"}>Female</MenuItem>
-                                            <MenuItem value={"male"}>Male</MenuItem>
-                                            <MenuItem value={"nonbinary"}>Nonbinary</MenuItem>
 
-                                        </Select>
-                                    </FormGroup>
-                                </Grid>
-                                <Grid item xs={2}>
-                                    <TextField
-                                        id="outlined-number"
-                                        value={prop.ageRange.low}
-                                        type="number"
-                                        InputLabelProps={{
-                                            shrink: true,
-                                        }}
-                                    />
-                                </Grid>
-                                <Grid item xs={2}>
-                                    <TextField
-                                        id="outlined-number"
-                                        value={prop.ageRange.high}
-                                        type="number"
-                                        InputLabelProps={{
-                                            shrink: true,
-                                        }}
-                                    />
-                                </Grid>
-                            </Grid>
-                            <Grid container spacing={2} style={{ marginTop: '1em' }}>
-                                <Grid item xs={2}>
-                                    <Box
-                                    
-                                        sx={{
-                                            width: 150,
-                                            height: 170,
-                                            backgroundColor: 'beige',
-                                        }}
-                                    >
-                                        <img src={prop.image} alt="picture" />
-                                    </Box>
-                                </Grid>
-                                <Grid item xs={3} md={6}>
-                                    <TextField
-                                        value={prop.description}
-                                        //value={project.description}, onChange allows edits
-                                        multiline
-                                        rows={5}
-                                        maxRows={8}
-                                        
-                                        fullWidth
-                                    />
-                                </Grid>
-                              
-                            </Grid>
-                            </div>
+
+const Character = (props) => {
+    let {ageRange, characterName, description, genderIdentity, image, type} = props.characters[props.index][1] || {};
+    // let high = ageRange.high || 0
+    // let low = ageRange.low || 0
+    const setCharacters = props.setCharacters;
+
+    const [tempCharacterName, setTempCharacterName] = useState(characterName);
+    const [tempDescription, setTempDescription] = useState(description)
+    const [tempGenderIdentity, setTempGenderIdentity] = useState(genderIdentity)
+    const [tempType, setTempType] = useState(type)
+    // const [tempLow, setTempLow] = useState(low)
+    // const [tempHigh, setTempHigh] = useState(high)
+  
+    const handleType = (event, type) => {
+        setTempType(type);
+      };
+
+    const handleGender = (event) => {
+        setTempGenderIdentity(event.target.value)
+    }
+    
+
+    const updateCharas = () => {
+        const characters = [...props.characters]; // Get a copy of the characters array
+        characters[props.index].splice(1, 1, {
+            characterName: tempCharacterName,
+            description: tempDescription,
+            genderIdentity: tempGenderIdentity,
+            type: tempType,
+            //ageRange.high: tempHigh
+        });
+
+        // Update the parent state
+        setCharacters(characters);
+
+    }
+    
+
+    return (
+        <div>
+            <Grid container spacing={5}>
+                <Grid item xs={2}>
+                    <TextField
+                        value={tempCharacterName}
+                        onChange={(e) => setTempCharacterName(e.target.value)}
+                    >
+                    </TextField>
+                    <button onClick={() => updateCharas()}>SAVE?</button>
+
+                </Grid>
+                <Grid item xs={2}>
+                    <ToggleButtonGroup
+                        value={tempType}
+                        exclusive
+                        onChange={handleType}
+                        aria-label="text alignment">
+                        <ToggleButton value="lead" aria-label="left aligned">
+                            Lead
+                        </ToggleButton>
+                        <ToggleButton value="supporting" aria-label="centered">
+                            Supporting
+                        </ToggleButton>
+                    </ToggleButtonGroup>
+                </Grid>
+                <Grid item xs={2}>
+                    <FormGroup fullWidth>
+                        <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            value={tempGenderIdentity}
+                            onChange={handleGender}
+                            label="Gender"
+                        >
+                            <MenuItem value={"female"}>Female</MenuItem>
+                            <MenuItem value={"male"}>Male</MenuItem>
+                            <MenuItem value={"nonbinary"}>Nonbinary</MenuItem>
+
+                        </Select>
+                    </FormGroup>
+                </Grid>
+                <Grid item xs={2}>
+                    <TextField
+                        id="outlined-number"
+                        // value={tempLow}
+                        // onChange={(e) => setTempLow(e.target.value)}
+                        // type="number"
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                    />
+                </Grid>
+                <Grid item xs={2}>
+                    <TextField
+                        id="outlined-number"
+                        // value={tempHigh}
+                        type="number"
+                        // onChange={(e) => setTempHigh(e.target.value)}
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                    />
+                </Grid>
+            </Grid>
+            <Grid container spacing={2} style={{ marginTop: '1em' }}>
+                <Grid item xs={2}>
+                    <Box
+
+                        sx={{
+                            width: 150,
+                            height: 170,
+                            backgroundColor: 'beige',
+                        }}
+                    >
+                        <img src={props.image} alt="picture" />
+                    </Box>
+                </Grid>
+                <Grid item xs={3} md={6}>
+                    <TextField
+                        value={tempDescription}
+                        onChange={(e) => setTempDescription(e.target.value)}
+                        multiline
+                        rows={5}
+                        fullWidth
+                    />
+                </Grid>
+
+            </Grid>
+        </div>
     )
+
 }
 
 export default Character;
--- src/pages/components/Project.js
+++ src/pages/components/Project.js
@@ -1,6 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 import Grid from '@mui/material/Grid';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
@@ -26,10 +31,29 @@ import EmailModal from './../EmailModal'
 import Character from './Character.js'
 import sampleProj from './project.json'
 
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
+
 export default function Project() {
     const [characters, setCharacters] = useState(
         Object.entries(sampleProj.PushId.characters.list));
+    const [projectDetails, setProjectDetails] = useState(
+       sampleProj.PushId.details
+    )
 
+    const [projectTags, setProjectTags] = useState(
+        sampleProj.PushId.details.tags.list.PushId
+     )
     const addChara = () => {
         let newChara = [
             "PushId3", {
@@ -44,20 +68,27 @@ export default function Project() {
                 type: "",
             }];
         setCharacters(characters => [...characters, newChara])
+        
+    }
+  
+    const removeChara = (index) => 
+    {
+        setCharacters(characters.filter((_, i) => i !== index))
+    };
+
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => {
+        setOpen(true);
+    };
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    useEffect(() =>
+    {
         console.log(characters)
-        console.log("new chara")
-    }
-    // const removeItem = (id) => {
-    //     setProduct(product.filter((i)=>(i.id !== id)))
+    }, [characters])
 
-    //   }
-    const removeChara = (id) => {
-        const index = characters.findIndex(chara => chara[0] === id); //use id instead of index
-        if (index > -1) { //make sure you found it
-            setCharacters(prevState => prevState.splice(index, 1));
-        }
-
-    }
 
     return (
         <div>
@@ -65,9 +96,17 @@ export default function Project() {
                 <Toolbar sx={{ marginLeft: '18em' }}>
                     <Grid container spacing={3}>
                         <Grid item xs={3}>
-                            <Typography variant="h6" noWrap component="div">
-                                {sampleProj.PushId.details.projectName}
-                            </Typography>
+                            <TextField 
+                                value={projectDetails.projectName}
+                                onChange={(e) =>
+                                    (prevState => ({
+                                        projectDetails: {                   // object that we want to update
+                                            ...prevState.projectDetails,    // keep all other key-value pairs
+                                            projectName: e.target.value       // update the value of specific key
+                                        }
+                                    }))}  
+                            >
+                            </TextField>
                         </Grid>
                         <Grid item xs={3}>
                             <Typography>
@@ -89,10 +128,8 @@ export default function Project() {
                     <Grid item xs={6} md={8}>
                         <TextField
                             value={sampleProj.PushId.details.description}
-
                             multiline
                             rows={10}
-                            maxRows={15}
                             fullWidth
                         />
                     </Grid>
@@ -115,11 +152,12 @@ export default function Project() {
                 <h2>Characters</h2>
                 <Table>
                     <TableBody>
-                        {characters.map((chara) => (
+                        {characters.map((chara, index) => (
                             <TableRow>
-                                <Character key={chara[0]} prop={chara[1]} />
-                                <Button sx={{ backgroundColor: "#DED7C3", marginBottom: '2em' }} variant="contained" onClick={removeChara}>Remove Character</Button>
-
+                                {/* <Character setCharacters={setCharacters} key={chara[0]} props={chara[1]} /> */}
+                                <Character characters={characters} setCharacters={setCharacters} key={index} index={index} />
+                                <Button sx={{ backgroundColor: "#DED7C3", marginBottom: '2em' }} variant="contained" onClick={() => removeChara(index)}>Remove Character</Button>
+                                
                             </TableRow>
                         ))
                         }
