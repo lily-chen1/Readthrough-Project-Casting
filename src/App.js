d5acf2a387f5d641bc6174a69dcb182f6e542360
tinie doan <77512550+tinietdoan@users.noreply.github.com>
2023-03-13 17:20:29 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { ProSidebarProvider } from 'react-pro-sidebar';
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ProjectPage from "./pages/ProjectPage";
+import ProjectDashboard from "./pages/ProjectDashboard.js";
 
 
 const App = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/projectPage" element={<ProjectPage />} />
+      <Route path="/projectDashboard" element={<ProjectDashboard />} />
     </Routes>
   </BrowserRouter>
 );
